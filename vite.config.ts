554544dac39c2f0d53756788bde6634d43b1a45b
JPThrives <boruahjp1999@gmail.com>
2024-12-30 22:26:00 +0530

    add: deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/cardio-disease-detection/'
+  plugins: [
+    react()],
+  base: '/cardio-disease-detection/',  // Replace <repo-name> with your actual GitHub repo name
+  server: {
+    proxy: {
+      '/predict': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
